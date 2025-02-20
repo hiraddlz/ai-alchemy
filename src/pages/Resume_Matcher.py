f2f0f42dd68fdfde8fda4f8822efdf17030b5fd7
hiraddlz <hirad.dlz@gmail.com>
2025-02-19 21:39:31 -0800

    add cover letter

--- src/pages/Resume_Matcher.py
+++ src/pages/Resume_Matcher.py
@@ -53,23 +53,34 @@ Job Description:
     return response
 
 
+# Initialize session state variables if they don't exist
+if "resume_text" not in st.session_state:
+    st.session_state.resume_text = ""
+if "job_description" not in st.session_state:
+    st.session_state.job_description = ""
+if "result" not in st.session_state:
+    st.session_state.result = None
+if "cover_letter" not in st.session_state:
+    st.session_state.cover_letter = ""
+
+
 # Streamlit UI
 st.set_page_config(layout="wide")
 st.title("📄Resume Matcher")
-st.subheader("Match resumes with job descriptions using AI analysis")
+st.subheader("Match resumes with job descriptions using AI")
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.header("Upload Resume")
     uploaded_file = st.file_uploader("Upload PDF or DOCX", type=["pdf", "docx"])
-    resume_text = ""
+    # resume_text = ""
 
     if uploaded_file is not None:
         if uploaded_file.name.endswith(".pdf"):
-            resume_text = extract_text_from_pdf(uploaded_file)
+            st.session_state.resume_text = extract_text_from_pdf(uploaded_file)
         elif uploaded_file.name.endswith(".docx"):
-            resume_text = extract_text_from_docx(uploaded_file)
+            st.session_state.resume_text = extract_text_from_docx(uploaded_file)
 
     # Manual resume input
     # resume_text_manual = st.text_area("Or write your resume manually:", height=300)
@@ -78,37 +89,66 @@ with col1:
 
 with col2:
     st.header("Job Description")
-    job_description = st.text_area("Paste the job description here:", height=400)
+    job_desc_input = st.text_area("Paste the job description here:", height=400)
+    st.session_state.job_description = job_desc_input
 
 
 if st.button(
     "Match", help="Analyze the resume and job description", use_container_width=True
 ):
-    if resume_text and job_description:
-        result = match_resume_to_job(resume_text, job_description)
-        if result["match_score"] >= "80%":
+    if st.session_state.resume_text and st.session_state.job_description:
+        with st.spinner("Evaluating resume..."):
+            st.session_state.result = match_resume_to_job(
+                st.session_state.resume_text, st.session_state.job_description
+            )
+        if st.session_state.result["match_score"] >= "80%":
             st.success(
-                f"Match Score: {result['match_score']} \n    🎉Perfect match! 🎉"
+                f"Match Score: {st.session_state.result['match_score']} \n    🎉Perfect match! 🎉"
             )
         else:
-            st.warning(f"Match Score: {result['match_score']}")
+            st.warning(f"Match Score: {st.session_state.result['match_score']}")
 
         st.write("### Revised Professional Summary:")
-        st.write(result["revised_summary"])
+        st.write(st.session_state.result["revised_summary"])
         st.write("### Phrases to Adjust:")
-        for original, improved in result["resume_phrases_to_adjust"].items():
+        for original, improved in st.session_state.result[
+            "resume_phrases_to_adjust"
+        ].items():
             diff = Redlines(original, improved)
             st.markdown(f"- {diff.output_markdown}", unsafe_allow_html=True)
 
         col1, col2 = st.columns(2)
         with col1:
             st.write("### Skills to Add:")
-            for skill in result["skills_to_add"]:
+            for skill in st.session_state.result["skills_to_add"]:
                 st.write(f"- :green[{skill}]")
         with col2:
             st.write("### Skills to Remove:")
-            for skill in result["skills_to_remove"]:
+            for skill in st.session_state.result["skills_to_remove"]:
                 st.write(f"- :red[{skill}]")
 
     else:
         st.warning("Please provide both resume and job description.")
+
+if st.button(
+    "Generate Cover Letter",
+    help="Generate a cover letter based on the resume and job description",
+    use_container_width=True,
+):
+    if st.session_state.resume_text and st.session_state.job_description:
+
+        system_prompt = """Generate a cover letter based on the resume and job description. Return the cover letter as a string."""
+        user_prompt = f"""Resume:\n
+{st.session_state.resume_text}
+Job Description:\n
+{st.session_state.job_description}"""
+        with st.spinner("Generating Cover letter..."):
+            st.session_state.cover_letter = generate_text(system_prompt, user_prompt)
+        st.write("#### Generated Cover Letter:")
+        st.write(st.session_state.cover_letter)
+        st.download_button(
+            label="Download Cover Letter",
+            data=st.session_state.cover_letter,
+            file_name="cover_letter.txt",
+            mime="text/plain",
+        )
