34987ded530a4e0f90d3881536501ce436cd6631
hiraddlz <hirad.dlz@gmail.com>
2025-02-25 23:23:23 -0800

    fix ascii artist ui

--- src/pages/Ascii_Artist.py
+++ src/pages/Ascii_Artist.py
@@ -1,17 +1,27 @@
 import streamlit as st
+
 from tools.llm_utils import generate_text
 
 
-def ascii_artist():
-    st.title("ASCII Artist")
+def main():
+    """ASCII Art Generator App."""
 
-    # Memory input
+    st.title("🎨 ASCII Art Generator 🖼️")
+    st.write("✨ Describe an object, and AI will create ASCII art for you. ✨")
+
     with st.form("ascii_artist_form"):
-        input_text = st.text_area("Enter the object you want to draw in ASCII art:")
-        if st.form_submit_button("Generate ASCII Art"):
-            system_prompt = "I want you to act as an ascii artist. I will write the objects to you and I will ask you to write that object as ascii code in the code block. Write only ascii code. Do not explain about the object you wrote. I will say the objects in double quotes"
-            ascii = generate_text(system_prompt, input_text)
-            st.code(ascii, language=None)
+        input_text = st.text_area("✍️ Enter the object you want to draw in ASCII art:", height=150, value='Dog!')
+        submitted = st.form_submit_button("🚀 Generate ASCII Art")
 
+        if submitted:
+            if input_text:
+                with st.spinner("🔄 Generating ASCII art..."):
+                    system_prompt = """I want you to act as an ascii artist. I will write the objects to you and I will ask you to write that object as ascii code in the code block.
+                    Write only ascii code. Do not explain about the object you wrote."""
+                    ascii_art = generate_text(system_prompt, input_text)
+                    st.code(ascii_art, language=None)
+            else:
+                st.warning("⚠️ Please enter an object to generate ASCII art. ⚠️")
 
-ascii_artist()
+if __name__ == '__main__':
+    main()
